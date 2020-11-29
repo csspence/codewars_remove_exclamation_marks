4fb6288be9d29e9d650b74760bd971c597ff8fce
Crew Spence <crewspence@gCrewSpence.local>
2020-11-29 05:52:22 -0600

    Wrote working solution.

--- codewars_remove_exclamation_marks.js
+++ codewars_remove_exclamation_marks.js
@@ -2,6 +2,6 @@
 Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 */
 
-function removeExclamationMarks(s) {
-  return '';
+const removeExclamationMarks = (s) => {
+  return (s.split('!')).join('');
 }
